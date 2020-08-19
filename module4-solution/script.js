ae6646ef7ae28f38b1e987eb2c217d724cc0f25e
Irina <iyakovetskaya@nes.ru>
2020-08-19 15:09:33 +0300

    j

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -60,7 +60,7 @@ WARNING!!! WARNING!!!
     // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
     // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
     // name in the loop.
-    if (firstLetter=='l') {
+    if (firstLetter=='j') {
       byeSpeaker.speak(names[name]);
     } else {
       helloSpeaker.speak(names[name]);
